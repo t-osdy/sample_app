2c19b63ad997dd098afe4f83683e103a89d79874
tosada <tomoya.a.osada@gmail.com>
2016-05-15 23:29:42 +0900

    add about pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "shoul get about" do
+    get :about
+    assert_response :success
+  end
+
 end
