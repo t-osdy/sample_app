dbf2bd709100035cc6ce6218c7517cf3f8b1c328
tosada <tomoya.a.osada@gmail.com>
2016-05-24 22:53:50 +0900

    add contact

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App" 
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -13,10 +13,15 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "shoul get about" do
+  test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
