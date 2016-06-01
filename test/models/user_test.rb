827c70b4fa9f79f5395447368db80af1e888e566
tosada <tomoya.a.osada@gmail.com>
2016-06-02 00:15:58 +0900

    add validates password length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,12 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email:"user@example.com")
+    @user = User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+      )
   end
 
   test "should be valid" do
@@ -31,8 +36,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                          foo@bar_baz.com foo@bar+baz.com]
+   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
    invalid_addresses.each do |invalid_address|
      @user.email = invalid_address
      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -45,7 +49,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
 
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 
 
 
