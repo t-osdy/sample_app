869d916ea1a7353a35f67d1ae6f2501533c34695
tosada <tomoya.a.osada@gmail.com>
2016-06-01 23:14:42 +0900

    add validates email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,15 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+
 end
