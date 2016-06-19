6de368b27c5ab90c0192c90b8bc6148217a26d27
tosada <tomoya.a.osada@gmail.com>
2016-06-19 22:05:36 +0900

    finish login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::password.create(string, cost: cost)
+    BCrypt::Password.create(string, cost: cost)
   end
 
 end
